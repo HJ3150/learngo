ff53160ffd04f707610d2290da90922bc0eeef7c
HJ <snows159@gmail.com>
2020-07-13 23:03:02 +0900

    20200713 2302

--- learngo/job scrapper/main.go
+++ learngo/job scrapper/main.go
@@ -1,16 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python"
 
 func main() {
-	pages := getPages()
+	getPages()
 
 }
 
 func getPages() int {
 	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each()
 	return 0
 }
-asdfsdfsdghhjjjbranch
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
